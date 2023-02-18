a87b78c4e2ba4dc250cf41b8a05ca6fc266c80b4
Abhinav Kumar <abhinavkumar34002@gmail.com>
2023-02-18 17:31:32 +0530

    Initialized Project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+function App() {
+  
+
+  return (
+    <div className="App">
+      <h1 className='text-3xl text-purple-500 animate-pulse'>Namaste World</h1>
+    </div>
+  )
+}
+
+export default App
